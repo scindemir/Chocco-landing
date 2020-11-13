0d70a5859554dc4df02c5997410efad0be8d30eb
Sasha Cindemir <scindmr@gmail.com>
2020-11-13 23:40:41 +0300

    hamburger working

--- main.js
+++ main.js
@@ -2,23 +2,27 @@ let menuOpenBurger = (function (buttonClass, menuClass) {
   let button = document.querySelector(buttonClass);
   let menu = document.querySelector(menuClass);
   let body = document.querySelector('body');
+  let buttonClose = document.querySelector('.btn-close');
 
   let _toggleMenu = function (e) {
-    button.classList.toggle('button_container--active');
+    button.classList.toggle('hamburger--active');
     menu.classList.toggle('overlay--open');
     body.classList.toggle('body-active-menu');
+    buttonClose.classList.toggle('hamburger')
   }
   
 
   let addListeners = function () {
     button.addEventListener('click', _toggleMenu);
+    buttonClose.addEventListener('click', _toggleMenu);
   };
 
   return {
     open: addListeners,
   };
 
-})('#toggle', '#overlay');
+
+})('#toggle', '#overlay', '#close');
 
 console.log(menuOpenBurger);
 menuOpenBurger.open();
